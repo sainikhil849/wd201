c4911027d97fb6835febbb7ee168482c357f9508
sainikhil849 <151641946+sainikhil849@users.noreply.github.com>
2024-01-19 21:07:59 +0530

    Create 20220927162227-create-todo.js

---
+++ todo-app/migrations/20220927162227-create-todo.js
@@ -0,0 +1,45 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Todos", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notNull: true,
+          len: [5, 255], // Modified length validation
+        },
+      },
+      dueDate: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+        validate: {
+          notNull: true,
+        },
+      },
+      completed: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Todos");
+  },
+};
